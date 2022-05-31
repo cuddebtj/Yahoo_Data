ca681efac4b9a0dcaa94cd49370b0ce8c4ecd214
Tim Cuddeback <cuddebtj@gmail.com>
2022-05-30 18:45:53 -0600

    Balck format updates

--- scripts/yahooupdate_post.py
+++ scripts/yahooupdate_post.py
@@ -11,29 +11,33 @@ from dotenv import load_dotenv
 from pathlib import Path
 from datetime import datetime
 from gspread_pandas import Spread
+
 load_dotenv()
+
 
 def yahoo_update():
     path = "/home/cuddebtj/python-projects/Yahoo_Data"
 
     today = np.datetime64("today", "D")
-    nfl_dates_df = pd.read_csv(os.path.join(path,"nfl-weeks.csv"), parse_dates=["Start_Date", "End_Date"])
+    nfl_dates_df = pd.read_csv(
+        os.path.join(path, "nfl-weeks.csv"), parse_dates=["Start_Date", "End_Date"]
+    )
     end = nfl_dates_df[nfl_dates_df["End_Date"] >= today]
     nfl_week = end[end["Start_Date"] <= today]
     nfl_week = nfl_week["Week"].iloc[0]
 
-    weeks = list(range(nfl_week,0,-1))
+    weeks = list(range(nfl_week, 0, -1))
 
-    df_id = pd.read_csv(os.path.join(path,"ID.csv"), dtype="str")
+    df_id = pd.read_csv(os.path.join(path, "ID.csv"), dtype="str")
     season = "2021"
 
     league_id = df_id[df_id["season"] == season]["league_ID"].values[0]
     game_id = df_id[df_id["season"] == season]["game_ID"].values[0]
 
     game_code = "nfl"
-    data_dir = os.path.join(path,"data")
+    data_dir = os.path.join(path, "data")
     auth_dir = Path().cwd()
-    working_dir = os.path.join(path,"test")
+    working_dir = os.path.join(path, "test")
 
     consumer_key = os.getenv("yahoo_consumer_key")
     consumer_secret = os.getenv("yahoo_consumer_secret")
@@ -51,9 +55,9 @@ def yahoo_update():
         all_output_as_json=False,
         consumer_key=consumer_key,
         consumer_secret=consumer_secret,
-        browser_callback=browser_callback
+        browser_callback=browser_callback,
     )
-    
+
     team_info = yahoo_query.get_league_teams()
     team_json = []
     for t in range(len(team_info)):
@@ -71,20 +75,31 @@ def yahoo_update():
 
     week_stats = pd.DataFrame()
     for id_ in team_ids["team_id"]:
-        current_week_stats = yahoo_query.get_team_stats_by_week(id_, chosen_week="current")
+        current_week_stats = yahoo_query.get_team_stats_by_week(
+            id_, chosen_week="current"
+        )
         week_dict = json.loads(str(current_week_stats["team_points"]))
         week_df = pd.DataFrame(week_dict, index=[0])
         week_df["team_id"] = id_
-        week_df["name"] = team_ids["name"][int(id_)-1]
+        week_df["name"] = team_ids["name"][int(id_) - 1]
         week_stats = pd.concat([week_stats, week_df])
     week_stats.reset_index(drop=True, inplace=True)
     week_stats["GType"] = "-"
     week_stats["key"] = ""
     week_stats["TeamB"] = "-"
     week_stats["ScoreB"] = "-"
-    week_stats = week_stats[["key", "week", "GType", "name", "total", "TeamB", "ScoreB"]]
-    week_stats.rename(columns={"week": "Week", "name": "TeamA", "total": "ScoreA"}, inplace=True)
-    week_stats.replace(to_replace=["Wesley", "ryan", "Peter Billups"], value=["Wes", "Ryan", "Pete"], regex=True, inplace=True)
+    week_stats = week_stats[
+        ["key", "week", "GType", "name", "total", "TeamB", "ScoreB"]
+    ]
+    week_stats.rename(
+        columns={"week": "Week", "name": "TeamA", "total": "ScoreA"}, inplace=True
+    )
+    week_stats.replace(
+        to_replace=["Wesley", "ryan", "Peter Billups"],
+        value=["Wes", "Ryan", "Pete"],
+        regex=True,
+        inplace=True,
+    )
 
     spread = Spread("MoM 2021 Schedule", sheet="Schedule")
     old_df = spread.sheet_to_df()
@@ -93,12 +108,19 @@ def yahoo_update():
     new_df = pd.concat([old_df, week_stats])
     new_df.reset_index(drop=True, inplace=True)
     new_df.drop(["key"], axis=1, inplace=True)
-    spread.df_to_sheet(new_df, index=False, headers=False, start=(2,2), replace=False, fill_value="")
+    spread.df_to_sheet(
+        new_df, index=False, headers=False, start=(2, 2), replace=False, fill_value=""
+    )
 
-    with open(path +"/cronoutput.txt", "a") as f:
-        f.write("Done: Post season update on " + datetime.now().strftime("%d/%m/%Y %H:%M:%S") + "\n")
+    with open(path + "/cronoutput.txt", "a") as f:
+        f.write(
+            "Done: Post season update on "
+            + datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+            + "\n"
+        )
 
     print(datetime.now().strftime("%d/%m/%Y %H:%M:%S"))
-    
+
+
 if __name__ == "__main__":
     yahoo_update()
--- scripts/yahooupdate_reg.py
+++ scripts/yahooupdate_reg.py
@@ -11,29 +11,33 @@ from dotenv import load_dotenv
 from pathlib import Path
 from datetime import datetime
 from gspread_pandas import Spread
+
 load_dotenv()
+
 
 def yahoo_update():
     path = "/home/cuddebtj/python-projects/Yahoo_Data"
 
     today = np.datetime64("today", "D")
-    nfl_dates_df = pd.read_csv(os.path.join(path,"nfl-weeks.csv"), parse_dates=["Start_Date", "End_Date"])
+    nfl_dates_df = pd.read_csv(
+        os.path.join(path, "nfl-weeks.csv"), parse_dates=["Start_Date", "End_Date"]
+    )
     end = nfl_dates_df[nfl_dates_df["End_Date"] >= today]
     nfl_week = end[end["Start_Date"] <= today]
     nfl_week = nfl_week["Week"].iloc[0]
 
-    weeks = list(range(nfl_week,0,-1))
+    weeks = list(range(nfl_week, 0, -1))
 
-    df_id = pd.read_csv(os.path.join(path,"ID.csv"), dtype="str")
+    df_id = pd.read_csv(os.path.join(path, "ID.csv"), dtype="str")
     season = "2021"
 
     league_id = df_id[df_id["season"] == season]["league_ID"].values[0]
     game_id = df_id[df_id["season"] == season]["game_ID"].values[0]
 
     game_code = "nfl"
-    data_dir = os.path.join(path,"data")
+    data_dir = os.path.join(path, "data")
     auth_dir = Path().cwd()
-    working_dir = os.path.join(path,"test")
+    working_dir = os.path.join(path, "test")
 
     consumer_key = os.getenv("yahoo_consumer_key")
     consumer_secret = os.getenv("yahoo_consumer_secret")
@@ -51,13 +55,13 @@ def yahoo_update():
         all_output_as_json=False,
         consumer_key=consumer_key,
         consumer_secret=consumer_secret,
-        browser_callback=browser_callback
+        browser_callback=browser_callback,
     )
-    
+
     weeks_df = pd.DataFrame()
-    
+
     for week in weeks:
-        
+
         scoreboard = yahoo_query.get_league_scoreboard_by_week(week)
 
         json_ = json.loads(str(scoreboard))
@@ -73,26 +77,49 @@ def yahoo_update():
         fb_df = pd.concat([teamsa2_df, teamsb2_df, match_type], axis=1)
 
         f_df = pd.concat([fa_df, fb_df]).reset_index(drop=True)
-        f_df.insert(1,"GType", 
-                    np.where(f_df["matchup.is_consolation"] == 1, "Toilet", 
-                             np.where(f_df["matchup.is_playoffs"] == 1, "Playoffs", "Reg")))
+        f_df.insert(
+            1,
+            "GType",
+            np.where(
+                f_df["matchup.is_consolation"] == 1,
+                "Toilet",
+                np.where(f_df["matchup.is_playoffs"] == 1, "Playoffs", "Reg"),
+            ),
+        )
 
-        cols = ["team.team_points.week.a_team", "GType", 
-                "team.managers.manager.nickname.a_team", "team.team_points.total.a_team", 
-                "team.managers.manager.nickname.b_team", "team.team_points.total.b_team"]
+        cols = [
+            "team.team_points.week.a_team",
+            "GType",
+            "team.managers.manager.nickname.a_team",
+            "team.team_points.total.a_team",
+            "team.managers.manager.nickname.b_team",
+            "team.team_points.total.b_team",
+        ]
 
         final_df = f_df[cols]
         final_df.columns = ["Week", "GType", "TeamA", "ScoreA", "TeamB", "ScoreB"]
-        final_df.replace(to_replace=["Wesley", "ryan", "Peter Billups"], value=["Wes", "Ryan", "Pete"], regex=True, inplace=True)
+        final_df.replace(
+            to_replace=["Wesley", "ryan", "Peter Billups"],
+            value=["Wes", "Ryan", "Pete"],
+            regex=True,
+            inplace=True,
+        )
         weeks_df = pd.concat([final_df, weeks_df])
-    
-    spread = Spread("MoM 2021 Schedule", sheet="Schedule")
-    spread.df_to_sheet(weeks_df, index=False, headers=False, start=(2,2), replace=False, fill_value="")
 
-    with open(path +"/cronoutput.txt", "a") as f:
-        f.write("Done: Regular season update on " + datetime.now().strftime("%d/%m/%Y %H:%M:%S") + "\n")
+    spread = Spread("MoM 2021 Schedule", sheet="Schedule")
+    spread.df_to_sheet(
+        weeks_df, index=False, headers=False, start=(2, 2), replace=False, fill_value=""
+    )
+
+    with open(path + "/cronoutput.txt", "a") as f:
+        f.write(
+            "Done: Regular season update on "
+            + datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+            + "\n"
+        )
 
     print(datetime.now().strftime("%d/%m/%Y %H:%M:%S"))
-    
+
+
 if __name__ == "__main__":
     yahoo_update()
